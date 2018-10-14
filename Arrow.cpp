15d465589f6a2803b32b9bcb70bb1ab90fd9575e
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2018-10-14 22:46:10 +0700

    Update Arrow.cpp

--- Arrow.cpp
+++ Arrow.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 int main()
 {
-	int a,b;
-	a=5 , b=3;
-	int c=5,d=3;
-	int max=5;
+	int a,b,max;
+	scanf("%d%d",&a,&b);
+	int c=a , d=b ;
+	if(a>b) max=a;
+	else max=b;
 	int op=max;
 	for(int i=1;i<=max;i++){
-		for(int j=1;j<=op-1;j++) printf("*");
+		for(int j=1;j<=op-1;j++) printf(" ");
 		if(c<d){
 			d--;
-			printf("-/");
+			printf(" /");
 		}
 		else if(c==d){
 			c--;
@@ -20,9 +21,26 @@ int main()
 		}
 		else if(c>d){
 			c--;
-			printf("/-");
+			printf("/ ");
 		}
 		op--;
+		printf("\n");
+	}
+	c=0 , d=0;
+	for(int i=1;i<=max;i++)
+	{
+		for(int j=1;j<i;j++) printf(" ");
+		if(c<d){
+			printf(" \\");
+		}
+		else if(c==d){
+			printf("\\\\");
+		}
+		else if(c>d){
+			printf("\\ ");
+		}
+		if(c<a-1) c++;
+		if(d<b-1) d++;
 		printf("\n");
 	}
 }
