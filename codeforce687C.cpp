05eea30682ca8f904c1aef6a52f18b67ce326af4
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2019-02-22 20:51:44 +0700

    Add files via upload

---
+++ codeforce687C.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using  namespace std;
+bool dp[505][505];
+int main()
+{
+	int n,make;
+	scanf("%d%d",&n,&make);
+	dp[0][0]=1;
+	while(n--)
+	{
+		int a;
+		scanf("%d",&a);
+		for(int i=make;i>=0;i--)
+		{
+			for(int j=make;j>=0;j--)
+			{
+				if(dp[i][j])
+					if(j+a<=make)
+					{
+						dp[i][j+a]=1;
+						dp[i+a][j+a]=1;
+					}
+			}
+		}
+	}
+	int cnt=0;
+	for(int i=0;i<=make;i++)
+	{
+		if(dp[i][make]) cnt++ ; 
+	}
+	printf("%d\n",cnt);
+	for(int i=0;i<=make;i++)
+	{
+		if(dp[i][make]) printf("%d ",i);
+	}
+}
